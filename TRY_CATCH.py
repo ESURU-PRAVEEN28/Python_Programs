14d751a9f9fdb6083ba985dd282b6df4756c67e9
unknown <DT023@DT023.tmfsmart.com>
2025-09-04 04:21:42 +0530

    another

---
+++ TRY_CATCH.py
@@ -0,0 +1,25 @@
+a,b=map(int,input().split())
+print("Enter the number for operation : \n 1)1->addition \n 2)2->substration \n 3)3->multiplication \n 4)4->division")
+c=int(input())
+match(c):
+    case 1:
+        print(a+b)
+        
+    case 2:
+        print(a-b)
+        
+    case 3:
+        print(a*b)
+        
+    case 4:
+        try:
+            d=a/b
+            print(d)
+        except ZeroDivisionError:
+            print("Zero DIVIDSION ERROR")
+        
+    case default:
+        print("\nplease enter a valid number:")
+        
+    
+    
